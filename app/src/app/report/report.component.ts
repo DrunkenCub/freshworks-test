a3bbda64bb98f4bbc24bca63eff837e4a39b52ee
Chathuranga Bandara <cbandara@wiley.com>
2021-07-28 00:47:02 +0530

    added paginator

--- app/src/app/report/report.component.ts
+++ app/src/app/report/report.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-report',
@@ -14,15 +15,22 @@ export class ReportComponent implements OnInit {
   dataSource: any = [];
   displayedColumns = ['user_id', 'location', 'food', 'fed_date', 'total_ducks', 'total_amount']
 
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
   ngOnInit(): void {
     this.dataService.getFeedingData(null).subscribe((data: any) => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
     })
   }
+ 
+  // applyFilter(event: Event) {
+  //   this.dataSource.filterPredicate = (data: any, filter: string) => {
+  //     return data.location == filter;
+  //   };
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  // }
 
 }
