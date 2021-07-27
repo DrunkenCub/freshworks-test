eaa34873466302c6c8b53275f3477f4071d6c81e
Chathuranga Bandara <cbandara@wiley.com>
2021-07-27 22:51:37 +0530

    added route guard and routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReportComponent } from './report/report.component';
+import { FeedComponent } from './feed/feed.component';
+import { LoginGuardGuard } from './login-guard.guard';
 
-const routes: Routes = [];
+//using https://angular.io/guide/router#preventing-unauthorized-access
+const routes: Routes = [
+  { path: '', redirectTo: 'feeder', pathMatch: 'full'},
+  { path: 'login', component:  LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'report', component:  ReportComponent,  canActivate:[LoginGuardGuard]},
+  { path: 'feeder', component:  FeedComponent,  canActivate:[LoginGuardGuard]},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/data.service.ts
+++ app/src/app/data.service.ts
@@ -6,7 +6,27 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
+    // Due to time restrictions I have only used one service class. Ideally for each domain there should be one service
+
   private REST_API_SERVER = "http://localhost:5000";
 
   constructor(private httpClient: HttpClient) { }
+
+  public login(email:string, password: string){
+    const login = {
+      'email': email,
+      'password': password
+    }
+    return this.httpClient.post(this.REST_API_SERVER + "/auth/login", login);
+  }
+  
+  public signup(email:string, password: string, fullname: string, admin: boolean){
+    const signup = {
+      'email': email,
+      'password': password,
+      'fullname': fullname,
+      'admin': admin
+    }
+    return this.httpClient.post(this.REST_API_SERVER + "/auth/signup", signup);
+  }
 }
